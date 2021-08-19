11fca2e3ee0191b6750a8148f0312f09966ed901
Saad Abdullah <iem.saad@hotmail.com>
2021-08-19 00:50:31 -0500

    updated gemfile.lock and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #create a main sample user
-# User.create!(user_name: "Example User",
-#              email: "example@railstutorial.org",
-#              password:              "foobar",
-#              password_confirmation: "foobar")
+User.create!(user_name: "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
 
-# 99.times do |n|
-#     user_name = Faker::Name.name
-#     email = "example-#{n+1}@railstutorial.org"
-#     password = "password"
-#     User.create!(user_name: user_name,
-#         email: email,
-#         password:              password,
-#         password_confirmation: password)
-#     end
+99.times do |n|
+    user_name = Faker::Name.name
+    email = "example-#{n+1}@railstutorial.org"
+    password = "password"
+    User.create!(user_name: user_name,
+        email: email,
+        password:              password,
+        password_confirmation: password)
+    end
 
 users = User.order(:created_at).take(6)
 50.times do 
@@ -30,11 +30,11 @@ users = User.order(:created_at).take(6)
 end
 
 
-# users = User.all
-# user = users.first
-# following = users[2..50]
-# followers = users[2..50]
-# following.each {|followed| user.follow(followed)}
-# followers.each {|follower| follower.follow(user)}
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[2..50]
+following.each {|followed| user.follow(followed)}
+followers.each {|follower| follower.follow(user)}
 
 
